626ed6b7eb946b83d8664a44ec887832af833b65
nila1313 <chaity13x@gmail.com>
2020-09-21 22:02:52 +0600

    assignment

--- services/learner_service/learner_courseService.php
+++ services/learner_service/learner_courseService.php
@@ -46,6 +46,27 @@ function getMyCourse()
 	return $courses;
 }
 
+function getInstructorCourse($id)
+{
+	$conn = dbConnection();
+
+    if(!$conn){
+        echo "DB connection error";
+    }
+  
+	
+    $sql = "select users.u_name,all_courses.course_name from learner_instructor join users join all_courses on learner_instructor.instructor_id=users.id and all_courses.course_id=learner_instructor.course_id where learner_instructor.learner_id='{$id}' ";
+	$result = mysqli_query($conn, $sql);
+	$courses = [];
+	while($row = mysqli_fetch_assoc($result)){
+		array_push($courses, $row);
+		
+	   
+	}
+	mysqli_close($conn);
+   
+	return $courses;
+}
 
 
 	
