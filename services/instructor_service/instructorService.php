79b0115b860fea5d3dbfcf4298646ff82894313d
SabikunnaharPyaasa <55408189+SabikunnaharPyaasa@users.noreply.github.com>
2020-09-22 16:30:56 +0600

    add

--- services/instructor_service/instructorService.php
+++ services/instructor_service/instructorService.php
@@ -57,6 +57,8 @@
 		$sql = "select * from users where id={$id}";
 		$result = mysqli_query($conn, $sql);
 		$row = mysqli_fetch_assoc($result);
+
+		//echo "".mysql_error($conn);
 		mysqli_close($conn);
 		return $row;
 	}
